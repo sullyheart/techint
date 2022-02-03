0076a593f8954cab16f89fe2b906dc5976f0af83
Abdulsalam Suliyat <sullieart@gmail.com>
2022-02-03 13:39:41 +0100

    add frontend files

---
+++ frontend/src/components/client-card.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: 'ClientCard',
+  props: ['client'],
+  data() {
+    return {
+      defaultBio: 'This user has no bios.'
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+.box
+  h1 {{ client.name }} ({{ client.age }})
+  p Bio: {{ client.bio || defaultBio }}
+  h2 Photos ({{ client.photos.length }})
+
+  .photo(v-for="photo in client.photos")
+    h3 {{ photo.filename }}
+    p(v-if="!photo.likedBy.length")
+      | no likes yet!
+    p(v-else)
+      | {{ photo.likedBy.map(client => client.name).join(', ') }}
+</template>
+
+<style lang="scss" scoped>
+.box {
+  padding: 2rem;
+  border: 1px solid #333;
+  background: #dd0;
+  border-radius: 0.3rem;
+}
+</style>
---
+++ frontend/src/views/About.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+  .about
+    h1This is an about page</h1>
+</template>
